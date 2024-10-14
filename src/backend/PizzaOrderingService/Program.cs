cdc027103f942ba97f841f86b92ff38613e2b223
Michael Eichhorn <kleines0eichhoernchen@posteo.de>
2024-10-14 22:07:23 +0200

    wip

--- src/backend/PizzaOrderingService/Program.cs
+++ src/backend/PizzaOrderingService/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using PizzaOrderingService.Data;
 using PizzaOrderingService.Services.HealthChecks;
 
@@ -16,12 +17,16 @@ internal static class Program
             .AddCheck<VersionInfoHealthCheck>("VersionInfo")
             .AddCheck<PizzaDbContextHealthCheck>("PizzaDbContext");
 
+        //builder.Services.ConfigureForwardedHeaders();
+
         builder.Services.AddPizzaDbContext(
             builder.Configuration
         );
 
         var app = builder.Build();
 
+        //app.UseForwardedHeaders();
+        //app.UseHttpLogging();
         app.ConfigureSwagger(builder.Configuration);
         app.MapControllers();
         app.MapHealthChecks("/health", new HealthCheckOptions
@@ -41,8 +46,7 @@ internal static class Program
     {
         if (configuration.GetValue<bool>("Swagger:Enabled"))
         {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            app.ConfigureSwaggerApp();
         }
 
         return app;
@@ -71,5 +75,41 @@ internal static class Program
     internal static async Task MigrateDatabaseAsync(PizzaDbContext dbContext)
     {
         await dbContext.Database.MigrateAsync();
+    }
+
+    //internal static IServiceCollection ConfigureForwardedHeaders(this IServiceCollection services)
+    //{
+    //    services.AddHttpLogging(options =>
+    //    {
+    //        options.LoggingFields = HttpLoggingFields.RequestPropertiesAndHeaders;
+    //    });
+
+    //    services.Configure<ForwardedHeadersOptions>(options =>
+    //    {
+    //        options.ForwardedHeaders =
+    //            ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+    //    });
+
+    //    return services;
+    //}
+
+    internal static WebApplication ConfigureSwaggerApp(this WebApplication app)
+    {
+        app.UseSwagger(c =>
+        {
+            c.PreSerializeFilters.Add((swaggerDoc, request) =>
+            {
+                if (request.Headers.TryGetValue("X-Forwarded-Prefix", out var serverPath))
+                {
+                    swaggerDoc.Servers = new List<OpenApiServer>()
+                    {
+                        new OpenApiServer() { Description = "Server behind", Url = serverPath }
+                    };
+                }
+            });
+        });
+        app.UseSwaggerUI();
+
+        return app;
     }
 }
