e05cc94e23a1f072f0f4c6d2a2431a39dbe800e2
Michael Eichhorn <kleines0eichhoernchen@posteo.de>
2024-10-17 06:38:02 +0200

    wip

--- src/backend/PizzaOrderingService/Program.cs
+++ src/backend/PizzaOrderingService/Program.cs
@@ -17,16 +17,12 @@ internal static class Program
             .AddCheck<VersionInfoHealthCheck>("VersionInfo")
             .AddCheck<PizzaDbContextHealthCheck>("PizzaDbContext");
 
-        //builder.Services.ConfigureForwardedHeaders();
-
         builder.Services.AddPizzaDbContext(
             builder.Configuration
         );
 
         var app = builder.Build();
 
-        //app.UseForwardedHeaders();
-        //app.UseHttpLogging();
         app.ConfigureSwagger(builder.Configuration);
         app.MapControllers();
         app.MapHealthChecks("/health", new HealthCheckOptions
@@ -77,22 +73,6 @@ internal static class Program
         await dbContext.Database.MigrateAsync();
     }
 
-    //internal static IServiceCollection ConfigureForwardedHeaders(this IServiceCollection services)
-    //{
-    //    services.AddHttpLogging(options =>
-    //    {
-    //        options.LoggingFields = HttpLoggingFields.RequestPropertiesAndHeaders;
-    //    });
-
-    //    services.Configure<ForwardedHeadersOptions>(options =>
-    //    {
-    //        options.ForwardedHeaders =
-    //            ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
-    //    });
-
-    //    return services;
-    //}
-
     internal static WebApplication ConfigureSwaggerApp(this WebApplication app)
     {
         app.UseSwagger(c =>
@@ -103,7 +83,7 @@ internal static class Program
                 {
                     swaggerDoc.Servers = new List<OpenApiServer>()
                     {
-                        new OpenApiServer() { Description = "Server behind", Url = serverPath }
+                        new OpenApiServer() { Description = "Pizza-Ordering-Service", Url = serverPath }
                     };
                 }
             });
