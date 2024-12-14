c469cca4d2298d66cc19b3b50900d618a0c8514b
Michael Eichhorn <kleines0eichhoernchen@posteo.de>
2024-12-14 10:18:53 +0100

    pipeline

---
+++ src/infrastructure/AWS/modules/aws_account/services.github-cicd-oidc.tf
@@ -0,0 +1,56 @@
+# based on https://medium.com/@thiagosalvatore/using-terraform-to-connect-github-actions-and-aws-with-oidc-0e3d27f00123
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com",
+  ]
+
+  thumbprint_list = ["ffffffffffffffffffffffffffffffffffffffff"]
+}
+
+data "aws_iam_policy_document" "github_oidc" {
+  # https://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles_create_for-idp_oidc.html#idp_oidc_Create_GitHub
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      values   = ["sts.amazonaws.com"]
+      variable = "token.actions.githubusercontent.com:aud"
+    }
+
+    condition {
+      test     = "StringLike"
+      values   = ["repo:medoni/2024-helm-microservice-deployment"]
+      variable = "token.actions.githubusercontent.com:sub"
+    }
+  }
+}
+
+resource "aws_iam_role" "github_deploy_role" {
+  name               = "github-oidc-deploy-role"
+  assume_role_policy = data.aws_iam_policy_document.github_oidc.json
+}
+
+data "aws_iam_policy_document" "github_deploy_policy" {
+  statement {
+    effect  = "Allow"
+    actions = [
+      "*",
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "deploy" {
+  name        = "github-oidc-deploy-policy"
+  description = "Policy used for deployments on CI from Github"
+  policy      = data.aws_iam_policy_document.github_deploy_policy.json
+}
