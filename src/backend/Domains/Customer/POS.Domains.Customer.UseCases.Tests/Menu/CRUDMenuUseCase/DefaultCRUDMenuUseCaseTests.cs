f36837f024cdd7f32c381f2090d08a38e3a6902e
Michael Eichhorn <meichhorn@conet.de>
2025-05-19 20:12:04 +0200

    wip

---
+++ src/backend/Domains/Customer/POS.Domains.Customer.UseCases.Tests/Menu/CRUDMenuUseCase/DefaultCRUDMenuUseCaseTests.cs
@@ -0,0 +1,10 @@
+﻿using POS.Shared.Testing;
+
+namespace POS.Domains.Customer.UseCases.Tests.Menu.CRUDMenuUseCase;
+
+[TestFixture]
+[Category(TestCategories.Unit)]
+public class DefaultCRUDMenuUseCaseTests
+{
+    private DefaultCRUDMenuUseCase Sut { get; set; } = null!;
+}
